4e5258798eaa96b19f9e5bb219311ab4fb57efe1
James Liu <jamesliu78@gmail.com>
2024-04-01 19:11:28 +0800

    Clear..

--- src/resources/views/advertising/slot.blade.php
+++ src/resources/views/advertising/slot.blade.php
@@ -1,17 +1,3 @@
-
-<!-- /21870649235/aces_all_bottom_mobile_special -->
-<div id='div-gpt-ad-1711532747722-0' style='min-width: 320px; min-height: 100px;'>
-    <script>
-        googletag.cmd.push(function () {
-            googletag.defineSlot('/21870649235/aces_all_bottom_mobile_special', [[320, 100], [1, 1]], 'div-gpt-ad-1711532747722-0')
-                .addService(googletag.pubads());
-        });
-        googletag.cmd.push(function () { googletag.display('div-gpt-ad-1711532747722-0'); });
-    </script>
-</div>
-
-
-
 <div class="tmgad tmgad-new">
     <script type="application/json">@json($config ?? [])</script>
 </div>
